511685b48a19a09995ccd8d8f1ffd475948d39cc
Mike Shon <michael_shon@yahoo.com>
2026-02-15 11:10:21 -0500

    Update main.py to set up network in connectToWifiAndUpdate()

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 
 
+# Based on MicroPython OTAUpdater, 
+# connectToWifiAndUpdate() below assumes that immediatly on reset, 
+# we want to connect to wifi,  check for update, and install it.
+#      CircuitPython will auto-connect to wifi on restart IF
+#      you have CIRCUITPY_WIFI_SSID and CIRCUITPY_WIFI_PASSWORD set up
+#      in your settings.toml file, but auto-connect is not necessary.
+#
+# There is also install_update_if_available_after_boot()
+# which checks to see if a previous connection was used
+# to discover that an update is available (bit not yet installed),
+# and does not initialize wifi if there is no update.
+
 def connectToWifiAndUpdate():
-    import time, machine, network, gc, app.secrets as secrets
+    import time,  gc
     time.sleep(1)
     print('Memory free', gc.mem_free())
 
     from app.ota_updater import OTAUpdater
 
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
-    otaUpdater = OTAUpdater('https://github.com/rdehuyss/micropython-ota-updater', main_dir='app', secrets_file="secrets.py")
+    
+
+    print('Memory free', gc.mem_free())
+    # If desired, you can pass the GitHub repo and other parameters here,
+    # but if you do not, they will be obtained from environment variables
+    # initialized from the settings.toml (default secrets) file in get_misc_settings()
+    settings = OTAUpdater.get_misc_settings()
+
+        # initialize network if not already active
+    OTAupdate._using_network(settings["wifi_ssid"], settings["wifi_password"))
+    
+    # Micropython OTAUpdater passed ( 'https://github.com/owner_name/repo_name', main_dir='app', secrets_file="secrets.py")
+    otaUpdater = OTAUpdater(settings = settings, github_repo = None)
+    
     hasUpdated = otaUpdater.install_update_if_available()
     if hasUpdated:
         machine.reset()
@@ -24,7 +41,7 @@ def connectToWifiAndUpdate():
         gc.collect()
 
 def startApp():
-    import app.start
+    import app.start        # sorry - this assumes that most code is in app/ subdir
 
 
 connectToWifiAndUpdate()
